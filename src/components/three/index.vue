9a532a1d7ff7bcb37ffd8009f148e8d38a0df322
dong0317 <lvchengdong@jingheoffice.com>
2024-02-22 11:25:24 +0800

    规范 创建先后顺序

--- src/components/three/index.vue
+++ src/components/three/index.vue
@@ -10,28 +10,8 @@ export default {
   },
   methods: {
     init() {
-      //创建一个场景
+      //1.创建一个场景
       const scene = new THREE.Scene();
-      // 创建相机  //常用的是透视相机
-      // 参数1：视野角度（FOV）
-      // 参数2：长宽比（aspect）
-      // 参数3：近裁剪面
-      // 参数4：远裁剪面
-      const camera = new THREE.PerspectiveCamera(
-        75,
-        window.innerWidth / window.innerHeight,
-        0.1,
-        1000
-      );
-      // camera.position.z = 5;
-      camera.position.set(0, 0, 5);
-     
-      // 创建渲染器
-      const render = new THREE.WebGLRenderer();
-      // 设置渲染器的大小
-      render.setSize(window.innerWidth, window.innerHeight);
-      // 将渲染器添加到DOM中
-      this.$refs.container.appendChild(render.domElement);
       // 创建一个 几何体
       const geometry = new THREE.BoxGeometry(1, 1, 1);
       // 创建一个材质
@@ -40,16 +20,37 @@ export default {
         wireframe: true,
       });
 
-      // 创建一个网格  将来 材质 集合体放进去
+      // 2.创建一个网格  将来 材质 集合体放进去
       const mesh = new THREE.Mesh(geometry, material);
       //设置网格模型在三维空间中的位置坐标，默认是坐标原点
       // mesh.position.set(-1, 1, -1);
       // 将网格添加到场景中
       scene.add(mesh);
-      //相机观察目标指向Threejs 3D空间中某个位置
+      // 3.创建相机  //常用的是透视相机
+        // 参数1：视野角度（FOV）
+        // 参数2：长宽比（aspect）
+        // 参数3：近裁剪面
+        // 参数4：远裁剪面
+      const camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+      );
+      // camera.position.z = 5;
+      camera.position.set(0, 0, 5);
+       //相机观察目标指向Threejs 3D空间中某个位置
       // camera.lookAt(0, 10, 0);  //y轴上位置10
       camera.lookAt(mesh.position);
+
+      // 4.创建渲染器
+      const render = new THREE.WebGLRenderer();
+      // 设置渲染器的大小
+      render.setSize(window.innerWidth, window.innerHeight);
+      // 将渲染器添加到DOM中
+      this.$refs.container.appendChild(render.domElement);
       // 渲染循环
+      // render.render(scene, camera);
       const animate = function() {
         // 在 Three.js 或其他动画场景中，通常会使用 requestAnimationFrame 来循环调用渲染函数，
         // 以便持续更新场景的动画效果，而不会导致性能问题或过度消耗资源。
@@ -57,8 +58,7 @@ export default {
         // 旋转立方体
         mesh.rotation.x += 0.01;
         mesh.rotation.y += 0.01;
-
-        render.render(scene, camera);
+        render.render(scene, camera); //执行一个渲染操作 类似于相机的拍照功能
       };
 
       animate();
