f074be2667d15af0e2a154fda262cca7a7278940
dong0317 <lvchengdong@jingheoffice.com>
2024-02-22 09:49:40 +0800

    1

--- src/components/three/index.vue
+++ src/components/three/index.vue
@@ -24,6 +24,7 @@ export default {
 
       // 创建渲染器
       const renderer = new THREE.WebGLRenderer();
+      // 浏览器视口大小
       renderer.setSize(window.innerWidth, window.innerHeight);
       this.$refs.container.appendChild(renderer.domElement);
 
