0f136abfa1e4ffe01cbe7c5a1b5b6bd677c191b6
dong0317 <lvchengdong@jingheoffice.com>
2024-02-22 09:45:48 +0800

    1

--- src/components/three/index.vue
+++ src/components/three/index.vue
@@ -15,7 +15,7 @@ export default {
 
       // 创建相机
       const camera = new THREE.PerspectiveCamera(
-        20,
+        75,
         window.innerWidth / window.innerHeight,
         0.1,
         1000
